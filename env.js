06b97205da28135dff8702735dd74ce0e0d97cea
notgro03 <gero.guerriero@gmail.com>
2025-10-07 11:23:30 -0700

    Deleted .env

--- env.js
+++
@@ -1,7 +0,0 @@
-// Rellena con tus credenciales públicas de Supabase
-// Puedes encontrarlas en Supabase > Settings > API
-// No subas este archivo a Git (está ignorado en .gitignore)
-window.__ENV__ = {
-  SUPABASE_URL: "https://ebezqrsgednjwhajddqu.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViZXpxcnNnZWRuandoYWpkZHF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ0NTEyNzYsImV4cCI6MjA1MDAyNzI3Nn0.6UpoIFJuEGDnLlD3_8w-fyQ2qMZ7uNDUttk-4Aeavgw"
-};
